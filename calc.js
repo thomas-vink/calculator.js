2fe5cabc8b7e347ce7f04fe48458841cfc5a54f2
MSI\thoma <thomasvink11@gmail.com>
2017-02-06 14:48:31 +0100

    variable toegvoegd

--- calc.js
+++ calc.js
@@ -1,7 +1,15 @@
 function btn(parms) {
 	register.innerHTML += parms.target.innerHTML;
 }
+function clear(){
+	register.innerHTML = "";
+}
 
+
+//clear
+document.getElementById("btnc").addEventListener("click",clear)
+
+//NUMBERS
 document.getElementById("btn0").addEventListener("click",btn);
 document.getElementById("btn1").addEventListener("click",btn);
 document.getElementById("btn2").addEventListener("click",btn);
@@ -12,11 +20,25 @@ document.getElementById("btn6").addEventListener("click",btn);
 document.getElementById("btn7").addEventListener("click",btn);
 document.getElementById("btn8").addEventListener("click",btn);
 document.getElementById("btn9").addEventListener("click",btn);
-//document.getElementById("btn+")addEventListener("click",btn);
-//document.getElementById("btn-").addEventListener("click",btn);
-//document.getElementById("btn/").addEventListener("click",btn);
-//document.getElementById("btn=").addEventListener("click",btn);
-//document.getElementById("btn*").addEventListener("click",btn);
+
+
+
+
+//Operators
+var operators;
+
+function op(){
+	var operators = 'op';
+	console.log ('operators')
+}
+
+
+document.getElementById("opr+").addEventListener("click",op);
+document.getElementById("opr-").addEventListener("click",op);
+document.getElementById("opr/").addEventListener("click",op);
+document.getElementById("opr=").addEventListener("click",op);
+document.getElementById("opr*").addEventListener("click",op);
+
 
 
 
